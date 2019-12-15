c93c7d67e049b1f6a397f936397cd2fb45084ccb
Marian Ban <marian.ban@gmail.com>
2019-12-15 20:58:54 +0100

    deploy

---
+++ precache-manifest.2d7567df983919fce9ef10f87cd8b34e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f53d09e629c852df455994924236476f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a0ec5aedb9c69d2b66e5",
+    "url": "/static/css/main.ccce7b32.chunk.css"
+  },
+  {
+    "revision": "ab6263e834ccc8c569ce",
+    "url": "/static/js/2.ef15c9f7.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.ef15c9f7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a0ec5aedb9c69d2b66e5",
+    "url": "/static/js/main.54a1ec96.chunk.js"
+  },
+  {
+    "revision": "63fe3645dbf1a7c563b9",
+    "url": "/static/js/runtime-main.a596ac53.js"
+  },
+  {
+    "revision": "4c1e171a2932644e29b9e031b4817689",
+    "url": "/static/media/Inter-UI-Regular.4c1e171a.ttf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5af07d78b26745c16ce6610956cc3b82.js"
+  "/precache-manifest.2d7567df983919fce9ef10f87cd8b34e.js"
 );
 
 self.addEventListener('message', (event) => {
