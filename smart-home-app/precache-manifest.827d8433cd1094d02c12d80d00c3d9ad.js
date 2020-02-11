e1c585d0aa559811700dfe204825df1ef825b775
Marian Ban <marian.ban@gmail.com>
2020-02-11 20:30:29 +0100

    smart home app added

---
+++ smart-home-app/precache-manifest.827d8433cd1094d02c12d80d00c3d9ad.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6b7d465d777fb3b5973c4821631db66",
+    "url": "/index.html"
+  },
+  {
+    "revision": "16d1480c110d8cfa4391",
+    "url": "/static/css/main.d135e94f.chunk.css"
+  },
+  {
+    "revision": "abc578a0cdd35c8456d1",
+    "url": "/static/js/2.e57098fa.chunk.js"
+  },
+  {
+    "revision": "eab49fb0b139d5f98d129da03039b637",
+    "url": "/static/js/2.e57098fa.chunk.js.LICENSE"
+  },
+  {
+    "revision": "16d1480c110d8cfa4391",
+    "url": "/static/js/main.b2867e59.chunk.js"
+  },
+  {
+    "revision": "2df55797556a78223318",
+    "url": "/static/js/runtime-main.a1e2b56b.js"
+  },
+  {
+    "revision": "4c1e171a2932644e29b9e031b4817689",
+    "url": "/static/media/Inter-UI-Regular.4c1e171a.ttf"
+  }
+]);
